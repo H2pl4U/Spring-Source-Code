545ff7af4fa58898e7eabc4d9cf51b2ac5ee5615
liuwei <wei.liu@shmedo.cn>
2021-03-29 17:02:16 +0800

    自动装配

---
+++ src/main/java/com/h2pl4u/spring/sourcecode/propertyValue/MainConfig4PropertyValues.java
@@ -0,0 +1,9 @@
+package com.h2pl4u.spring.sourcecode.propertyValue;
+
+/**
+ * Created on 2021/3/29 14:58
+ *
+ * @Author Liuwei
+ */
+public class MainConfig4PropertyValues {
+}
---
+++ src/test/java/com/h2pl4u/spring/sourcecode/propertyValue/PropertyValueTest.java
@@ -0,0 +1,9 @@
+package com.h2pl4u.spring.sourcecode.propertyValue;
+
+/**
+ * Created on 2021/3/29 14:59
+ *
+ * @Author Liuwei
+ */
+public class PropertyValueTest {
+}
